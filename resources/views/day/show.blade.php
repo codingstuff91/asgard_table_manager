cfd435c6f889c40ea81ed359a5183aa493451b6a
Matthieu MARTIN <codingstuff91@gmail.com>
2023-02-11 18:04:20 +0100

    Edited show day view

--- resources/views/day/show.blade.php
+++ resources/views/day/show.blade.php
@@ -9,29 +9,38 @@
     </x-slot>
 
     <div class="w-full mt-2 mx-auto lg:max-w-7xl">
-        <div class="grid grid-cols-2 lg:grid-cols-5 gap-1 rounded-lg mt-4 mx-auto">
+        <div class="grid grid-cols-2 gap-1 rounded-lg mt-4 mx-auto lg:grid-cols-5 lg:gap-2">
             @foreach ($tables as $table)
-                <div class="flex flex-col bg-white rounded-lg text-center shadow-lg lg:my-0 xs:my-4 xs:mx-2">
-                    <h2 class="text-xl font-semibold w-full bg-blue-200 p-2 rounded-lg">{{ $table->game->name }}</h2>
-                    <h3>Début : {{ $table->start_hour }}</h3>
-                    <h3>Maitre jeu : {{ $table->organizer->name }}</h3>
-                    <hr>
+                <div class="flex flex-col bg-white rounded-lg text-center shadow-lg xs:my-4 xs:mx-2">
+                    <h2 class="text-xl font-semibold w-full p-2 rounded-lg text-white {{ $table->game->category->color }}">{{ $table->game->name }}</h2>
+                    
+                    <h3 class="mt-2 font-bold text-lg">Début</h3>
+                    <p>{{ $table->start_hour }}</p>
+                    
+                    <h3 class="mt-2 font-bold text-lg">
+                        Maitre jeu
+                    </h3>
+                    <p class="mb-2">{{ $table->organizer->name }}</p>
 
-                    <h3 class="my-1 font-semibold">Joueurs inscrits :</h3>
-                    <ul class="mt-2">
-                        @foreach ($table->users as $user)
-                            <li>{{ $user->name }}</li>
-                        @endforeach
-                    </ul>
+                    <hr class="border border-gray-600">
+
+                    <div class="bg-gray-200 pb-4">
+                        <h3 class="mt-4 text-lg font-bold">Joueurs inscrits</h3>
+                        <ul class="mt-4">
+                            @foreach ($table->users as $user)
+                                <li>{{ $user->name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
 
                     <div>                          
                         @if (collect($table->users)->pluck('name')->doesntContain(Auth::user()->name))
-                            <button class="relative bottom-0 mt-2 px-4 py-2 bg-lime-300 rounded-lg w-full">
+                            <button class="relative bottom-0 px-4 py-2 bg-lime-300 w-full text-lg font-bold">
                                 <a href="{{ route('table.subscribe', [$table->id, Auth::user()->id]) }}">S'incrire</a>
                             </button>
                         @endif
                         @if (collect($table->users)->pluck('name')->contains(Auth::user()->name))
-                            <button class="relative bottom-0 mt-2 px-4 py-2 bg-red-500 text-white rounded-lg w-full">
+                            <button class="relative bottom-0 px-4 py-2 bg-lime-300 w-full text-lg font-bold">
                                 <a href="{{ route('table.unsubscribe', [$table->id, Auth::user()->id]) }}">Se désincrire</a>
                             </button>
                         @endif
