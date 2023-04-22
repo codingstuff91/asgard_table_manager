3684c74038e1a6416defb8fbd3bb4fbcbfc28f9e
Matthieu MARTIN <codingstuff91@gmail.com>
2023-04-22 21:50:59 +0200

    Delete DD

--- app/Listeners/TableCreatedDiscordNotification.php
+++ app/Listeners/TableCreatedDiscordNotification.php
@@ -37,14 +37,12 @@ class TableCreatedDiscordNotification
         // Samedi => 6
         // Dimanche => 0
         $dayOfWeek = $event->day->date->dayOfWeek;
-        
+
         $channels = [
             0 => "1069338721633194025",
             5 => "1069338626237931541",
             6 => "1069338674753437850",
         ];
-
-        // dd($channels[$day->date->dayOfWeek]);
 
         $message = "Une table de **". $game->name ."** a été crée par ". $event->user->name ."\n __**Date:**__ ". $event->day->date->format('d/m/Y') ." __**Heure:**__ ". $event->table->start_hour."\n Pour vous inscrire rendez vous sur ce lien : http://table-manager.jeuf5892.odns.fr/day/".$event->day->id;
 
