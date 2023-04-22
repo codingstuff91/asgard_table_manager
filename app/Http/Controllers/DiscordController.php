6b5c541af553c19b640d090bd32ce0cc9d06662d
Matthieu MARTIN <codingstuff91@gmail.com>
2023-04-22 16:29:51 +0200

    Change notification message

--- app/Http/Controllers/DiscordController.php
+++ app/Http/Controllers/DiscordController.php
@@ -13,13 +13,17 @@ class DiscordController extends Controller
         $client = new Client();
         $bot_token = config('discord.bot_token');
 
+        $game = "COC contemporain";
+        $creator = "mattou2812";
+        $date = "14/03/2023";
+
         $response = $client->post('https://discord.com/api/v9/channels/800385339737899018/messages', [
             'headers' => [
                 'Authorization' => $bot_token,
                 'Content-Type' => 'application/json'
             ],
             'json' => [
-                'content' => 'Ceci est un message de teste généré automatiquement avec un lien http://table-manager.jeuf5892.odns.fr'
+                'content' => 'Une table de '. $game .'  a été crée par ' . $creator . ' pour le '. $date .'. Plus d\'infos ici : http://table-manager.jeuf5892.odns.fr'
             ]
         ]);
 
