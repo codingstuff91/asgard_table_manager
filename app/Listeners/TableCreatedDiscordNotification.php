820eac37d64d0c9056393e0b3866c58500c1fe82
Matthieu MARTIN <codingstuff91@gmail.com>
2023-04-28 21:12:25 +0200

    Add example embed message

--- app/Listeners/TableCreatedDiscordNotification.php
+++ app/Listeners/TableCreatedDiscordNotification.php
@@ -46,6 +46,44 @@ class TableCreatedDiscordNotification
 
         $message = "Une table de **". $game->name ."** a été crée par ". $event->user->name ."\n __**Date:**__ ". $event->day->date->format('d/m/Y') ." __**Heure:**__ ". $event->table->start_hour."\n Pour vous inscrire rendez vous sur ce lien : http://table-manager.jeuf5892.odns.fr/days/".$event->day->id;
 
+        $embeddedMessage = [
+            {
+                "content": "Une table a été ouverte sur ASGARD-TABLE-MANAGER",
+                "embeds": [
+                    {
+                        "title": "Partie de XXXX",
+                        "description": "Plus d'informations sur http://table-manager.jeuf5892.odns.fr/days",
+                        "color": 65280,
+                        "author": {
+                            "name": "Par : Mattou"
+                        },
+                        "fields": [
+                            {
+                                "name": "Date",
+                                "value": "28/04/2023",
+                                "inline": true
+                            },
+                            {
+                                "name": "Heure",
+                                "value": "21:00",
+                                "inline": true
+                            },
+                            {
+                                "name": "Nombre joueurs",
+                                "value": 10,
+                                "inline": true
+                            },
+                            {
+                                "name": "Nombre points",
+                                "value": 10000,
+                                "inline": true
+                            }
+                        ]
+                    }
+                ]
+            }
+        ];
+        
         $response = $client->post("https://discord.com/api/v9/channels/". $channels[$dayOfWeek] ."/messages", [
             'headers' => [
                 'Authorization' => $bot_token,
