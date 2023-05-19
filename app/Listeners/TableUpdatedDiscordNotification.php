9b8169a95502eb659a4ccbefc30ea2e05aefbd58
Matt MARTIN <codingstuff91@gmail.com>
2023-05-19 15:06:23 +0200

    Refact TableUpdated listener

--- app/Listeners/TableUpdatedDiscordNotification.php
+++ app/Listeners/TableUpdatedDiscordNotification.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Models\Game;
 use GuzzleHttp\Client;
 use App\Events\TableUpdated;
+use App\Services\DiscordService;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -23,16 +24,7 @@ class TableUpdatedDiscordNotification
         $client = new Client();
         $bot_token = config('discord.bot_token');
 
-        // Vendredi => 5
-        // Samedi => 6
-        // Dimanche => 0
-        $dayOfWeek = $event->day->date->dayOfWeek;
-
-        $channels = [
-            0 => "1069338721633194025",
-            5 => "1069338626237931541",
-            6 => "1069338674753437850",
-        ];
+        $discordChannelId = resolve(DiscordService::class)->getChannelByDate($event->day->date);
 
         $tableLinkText = 'Plus d\'informations sur http://table-manager.jeuf5892.odns.fr/days/' . $event->day->id;
 
@@ -65,7 +57,7 @@ class TableUpdatedDiscordNotification
             ]
         ];
 
-        $response = $client->post("https://discord.com/api/v9/channels/". $channels[$dayOfWeek] ."/messages", [
+        $response = $client->post("https://discord.com/api/v9/channels/". $discordChannelId ."/messages", [
             'headers' => [
                 'Authorization' => $bot_token,
                 'Content-Type' => 'application/json'
