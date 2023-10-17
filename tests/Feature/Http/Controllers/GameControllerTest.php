249c05aed21ed8acaca2210eb5ecc29a8e14895b
matthieumartin2812 <120380267+matthieumartin2812@users.noreply.github.com>
2023-10-17 13:33:22 +0200

    Fixed duplicate game creation (#39)
    * Add unique validation rule for game creation
    
    * Add previous value on game creation form
    
    * Add feature test to avoid to store duplicate game
    
    ---------
    
    Co-authored-by: Matthieu MARTIN <matthieu.martinl@global-exam.com>

---
+++ tests/Feature/Http/Controllers/GameControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Day;
+use App\Models\Game;
+use App\Models\User;
+
+test('a game must not be created twice', function () {
+    $this->seed();
+    $this->actingAs(User::first());
+
+    $response = $this->post('/games', [
+        'name' => 'test game',
+        'category_id' => Category::first(),
+    ]);
+
+    expect(['name' => Game::first()->name])->toBeInDatabase('games');
+
+    $response = $this->post('/games', [
+        'name' => 'test game',
+        'category_id' => Category::first(),
+    ]);
+
+    $response->assertSessionHasErrors('name');
+});
