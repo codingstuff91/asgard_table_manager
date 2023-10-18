c20f2207c8fef7d54ce32e383854601217fe0a48
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-10-18 09:43:33 +0200

    Fix styles of TableDataObject

--- app/DataObjects/TableData.php
+++ app/DataObjects/TableData.php
@@ -2,9 +2,6 @@
 
 namespace App\DataObjects;
 
-use App\Http\Requests\TableStoreRequest;
-use Illuminate\Support\Facades\Auth;
-
 class TableData
 {
     public function __construct(
@@ -15,19 +12,19 @@ class TableData
         public int $total_points,
         public string $start_hour,
         public ?string $description = null,
-    ){
+    ) {
     }
 
     public function toArray(): array
     {
         return [
-            'organizer_id'   => $this->organizer_id,
-            'day_id'         => $this->day_id,
-            'game_id'        => $this->game_id,
+            'organizer_id' => $this->organizer_id,
+            'day_id' => $this->day_id,
+            'game_id' => $this->game_id,
             'players_number' => $this->players_number,
-            'total_points'   => $this->total_points,
-            'start_hour'     => $this->start_hour,
-            'description'    => $this->description ?? null,
+            'total_points' => $this->total_points,
+            'start_hour' => $this->start_hour,
+            'description' => $this->description ?? null,
         ];
     }
 }
