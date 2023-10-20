0eac14f2b8b7760505751f0ebff3a443a2eb10da
Matthieu MARTIN <codingstuff91@gmail.com>
2023-10-20 15:09:07 +0200

    Fix events and listeners coding styles

--- app/Events/UserTableUnsubscribed.php
+++ app/Events/UserTableUnsubscribed.php
@@ -2,15 +2,11 @@
 
 namespace App\Events;
 
-use App\Models\User;
 use App\Models\Table;
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
+use App\Models\User;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class UserTableUnsubscribed
 {
