816cbbfd51f9cb90ba8486adab869c2667fdd64b
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-10-27 22:08:24 +0200

    Add SendDiscordNotificationAction test

--- app/Actions/Discord/SendDiscordNotificationAction.php
+++ app/Actions/Discord/SendDiscordNotificationAction.php
@@ -6,16 +6,20 @@ use GuzzleHttp\Client;
 
 class SendDiscordNotificationAction
 {
+    public function __construct(private Client $client)
+    {
+    }
+
     public function __invoke(int $channelId, array $embedMessage)
     {
-        $client = new Client;
-
-        $client->post(config('discord.api_url').$channelId.'/messages', [
+        $this->client->post(config('discord.api_url').$channelId.'/messages', [
             'headers' => [
                 'Authorization' => config('discord.bot_token'),
                 'Content-Type' => 'application/json',
             ],
             'json' => $embedMessage,
         ]);
+
+        return "Discord notification sent";
     }
 }
