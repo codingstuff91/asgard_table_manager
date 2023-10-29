46ed57631e64bfe97243a47e9d49766bd0654f7c
Matthieu MARTIN <37110847+codingstuff91@users.noreply.github.com>
2023-10-29 15:58:47 +0100

    Refactorization of sending discord notification process (#45)
    * Extract sending notification process in discord service
    
    * Replace game_id by full Game Model and remove User Model
    
    * Edit table created event
    
    * Delete useless user parameter and replace game_id by Game full model
    
    * Extract build notification embed message in discord service
    
    * Extract title and content of discord notification
    
    * Improved buildEmbedNotification method
    
    * Improved discordService
    
    * Add unit test for setNotificationContent method
    
    * Coding styles cleanup
    
    * Refact discord service with new config variable
    
    * Refacto to manage different buildEmbed notification messages
    
    * Rename embed methods
    
    * Rename embed methods
    
    * Add EmbedColor to manage discord notif colors
    
    * Add DefineChannelIdAction class and unit test
    
    * Add DiscordNotificationData DTO
    
    * Add main CreateDiscordNotification action class to handle embed message creation and send the notification
    
    * Replace Table created event and its listener by discordNotificationData action class
    
    * Remove create and update events and listeners and replace by action class
    
    * Improve code
    
    * Remove discord service
    
    * Add unit test for BuildEmbedMessageStructure action class
    
    * Moved DefineChannelId unit test
    
    * Add SendDiscordNotificationAction test
    
    * Create Pest method to mock Http calls
    
    * Edit TableController test
    
    * Cleanup coding styles
    
    * Add method to manage embed color and description
    
    * Rename EmbedMessageTitle by EmbedMessageContent
    
    * add unsubscribe to handle buildEmbed structure
    
    * Replace User events and listeners by CreateDiscordNotificationAction
    
    * Remove duplicate values from EmbedColor enum
    
    * Add login pest method
    
    * Improve BuildEmbedMessageStructure and tests
    
    * Added missing DTO and clean useless action class import
    
    * Code styles cleanup
    
    * Rename methods names
    
    * Add pint composer command
    
    * Add comment
    
    * Improve SendDiscordNotificationAction test
    
    * Remove User parameter from subscribe and unsubscribe user methods
    
    ---------
    
    Co-authored-by: Matthieu MARTIN <matthieu.martinl@global-exam.com>

---
+++ app/Actions/Discord/CreateDiscordNotificationAction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Actions\Discord;
+
+use App\DataObjects\DiscordNotificationData;
+
+class CreateDiscordNotificationAction
+{
+    public function __construct(
+        public DefineChannelIdAction $defineChannelIdAction,
+        public BuildEmbedMessageStructureAction $buildEmbedMessageStructureAction,
+        public SendDiscordNotificationAction $sendDiscordNotificationAction,
+    ) {
+    }
+
+    public function __invoke(
+        DiscordNotificationData $discordNotificationData,
+        string $notificationType,
+    ): void {
+        $channelId = ($this->defineChannelIdAction)($discordNotificationData->day->date);
+
+        $embedMessage = $this->buildEmbedMessageStructureAction::buildEmbedStructure($discordNotificationData, $notificationType);
+
+        ($this->sendDiscordNotificationAction)($channelId, $embedMessage);
+    }
+}
