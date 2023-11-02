45666fa11c957c832367108cd467ab741f76d10e
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-11-02 22:46:26 +0100

    Coding styles cleanup

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,14 +16,16 @@ class DashboardController extends Controller
 
         $tables = Table::all()->count();
 
-        $afternoonTables = Table::all()->filter(function($value, $key) {
+        $afternoonTables = Table::all()->filter(function ($value, $key) {
             $hour = explode(':', $value->start_hour)[0];
-            return  $hour >= 13 && $hour <= 19 ;
+
+            return $hour >= 13 && $hour <= 19;
         })->count();
 
-        $eveningTables = Table::all()->filter(function($value, $key) {
+        $eveningTables = Table::all()->filter(function ($value, $key) {
             $hour = explode(':', $value->start_hour)[0];
-            return  $hour > 19 && $hour < 23 ;
+
+            return $hour > 19 && $hour < 23;
         })->count();
 
         return view('dashboard', compact('users', 'days', 'tables', 'afternoonTables', 'eveningTables'));
