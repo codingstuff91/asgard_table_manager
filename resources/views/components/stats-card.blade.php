58956b4d7ab666d7cd3a0ef01d716126e31f3b83
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-11-02 22:03:47 +0100

    Add new images and stat-card component

---
+++ resources/views/components/stats-card.blade.php
@@ -0,0 +1,9 @@
+<div class="py-2 bg-gray-700 rounded-lg">
+    <div class="flex justify-center items-center xs:flex-col">
+        <img src="{{ $image }}" alt="calendar logo" class="h-10 w-10">
+        <h2 class="mt-2 text-lg font-bold text-center text-gray-900 dark:text-white">
+            {{ $title }}
+        </h2>
+    </div>
+    <p class="mt-2 text-center text-xl dark:text-white">{{ $data }}</p>
+</div>
