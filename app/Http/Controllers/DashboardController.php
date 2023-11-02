5a584d6a40f70119a543d5a55b87fac1df88bba9
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-11-02 22:45:07 +0100

    Add stats to count tables for afternoon and evenings time periods

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,6 +16,16 @@ class DashboardController extends Controller
 
         $tables = Table::all()->count();
 
-        return view('dashboard', compact('users', 'days', 'tables'));
+        $afternoonTables = Table::all()->filter(function($value, $key) {
+            $hour = explode(':', $value->start_hour)[0];
+            return  $hour >= 13 && $hour <= 19 ;
+        })->count();
+
+        $eveningTables = Table::all()->filter(function($value, $key) {
+            $hour = explode(':', $value->start_hour)[0];
+            return  $hour > 19 && $hour < 23 ;
+        })->count();
+
+        return view('dashboard', compact('users', 'days', 'tables', 'afternoonTables', 'eveningTables'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,11 @@
                 <x-stats-card title="Sessions" :data="$days" image="{{ asset('img/calendar.png') }}"/>
                 <x-stats-card title="Tables" :data="$tables" image="{{ asset('img/game-table.png') }}"/>
             </div>
+            <h3 class="mt-4 text-lg font-bold text-center dark:text-white">Nombre tables par période</h3>
+            <div class="grid grid-cols-2 gap-2 rounded-lg mt-2 mx-auto lg:grid-cols-4 lg:gap-2">
+                <x-stats-card title="Après-midi (13h à 19h)" :data="$afternoonTables" image="{{ asset('img/sun.png') }}"/>
+                <x-stats-card title="Soirée (de 19h01 à Minuit)" :data="$eveningTables" image="{{ asset('img/moon.png') }}"/>
+            </div>
         </div>
     </div>
 </x-app-layout>
