936f217ce2bb1fb88ee141129d79d78aa0812131
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-11-10 10:14:28 +0100

    Add UserLogic class and unit tests

---
+++ app/Logic/UserLogic.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Logic;
+
+use App\DataObjects\DiscordNotificationData;
+use App\Models\Day;
+use App\Models\Table;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserLogic
+{
+    public function hasSubscribedToAnotherTableWithTheSameStartHour(Day $day, Table $table): bool
+    {
+        $tables = Table::where('start_hour', $table->start_hour)
+            ->where('day_id', $day->id)
+            ->where('id', '!=', $table->id)
+            ->get();
+
+        if($tables->isEmpty()) {
+            return false;
+        }
+
+        foreach($tables as $table) {
+            if ($table->users->contains(Auth::user())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
