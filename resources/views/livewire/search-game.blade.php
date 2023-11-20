0af2305a807e43d8e6e612630e7f4951d860a03c
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-11-20 13:31:30 +0100

    WIP search game component

--- resources/views/livewire/search-game.blade.php
+++ resources/views/livewire/search-game.blade.php
@@ -4,11 +4,12 @@
         type="text"
         wire:model.live="searchGameInput"
         class="w-full mt-4 border-2 border-red-500"
-        name="players_number"
-        placeholder="Nom du jeu. Par ex : Aliens">
+        name="game_search"
+        placeholder="Nom du jeu. Par ex : Aliens"
+        value="{{ old('game_search') }}">
     </x-text-input>
 
-    <select class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
+    <select class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" name="game_id">
         <option value="">{{ $gamesCount == 0 ? "Aucun jeu trouvé" : $gamesCount . " Jeux trouvés" }}</option>
         @foreach ($games as $game)
             <option value="{{ $game->id }}">{{ $game->name }}</option>
