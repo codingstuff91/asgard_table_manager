e4a8a87c9304b8273967f0495920c3607b003846
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2023-12-06 16:54:44 +0100

    Edit text input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-2 border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white focus:border-indigo-800 dark:focus:border-indigo-900 dark:focus:text-white focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-2 border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white focus:border-indigo-700 dark:focus:border-indigo-700 dark:focus:text-white focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
