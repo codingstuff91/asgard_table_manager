9642e28188d03b52e752972d0ec51800e667a83a
Matthieu MARTIN <codingstuff91@gmail.com>
2023-12-12 21:52:01 +0100

    Add links to documentation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,9 @@
                     <x-nav-link href="/admin">
                         {{ __('Admin') }}
                     </x-nav-link>
+                    <x-nav-link href="http://atm-documentation.jeuf5892.odns.fr/">
+                        {{ __('Documentation') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -75,6 +78,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link href="/admin">
                 {{ __('Admin') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="http://atm-documentation.jeuf5892.odns.fr/">
+                {{ __('Documentation') }}
             </x-responsive-nav-link>
         </div>
 
