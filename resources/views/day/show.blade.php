1c1b2a60c3e8aca62834aa58fbeeef09b552da95
matthieumartin2812 <120380267+matthieumartin2812@users.noreply.github.com>
2024-02-19 23:27:43 +0100

    Added event creation on day show page (#71)
    * Add pint json config
    
    * Update readme
    
    * Add event model related files
    
    * Add event create page
    
    * Add request and test
    
    * Display description validation message
    
    * Refacto event creation
    
    * Add start_hour column
    
    * Add events relationship
    
    * Create event_user pivot table
    
    * Add users relationship
    
    * Add day_id column
    
    * WIP event card component
    
    * Add edit event button and gate
    
    * Add delete event button and gate
    
    * Add subscribe and unsubscribe to an event features
    
    * Add available tables title
    
    * Improve action buttons and main header text
    
    * Event edit and update features
    
    * Dsiplay event number on days index view
    
    * Add test for edit and update event features
    
    * Edit text color for browser with light mode
    
    * Add test for subscribe or unsubscribe an user of an event
    
    * Delete useless imports
    
    * Edit styles
    
    * Edit font sizes
    
    * delete dd
    
    * delete useless seeder
    
    ---------
    
    Co-authored-by: Matthieu MARTIN <matthieu.martinl@global-exam.com>

--- resources/views/day/show.blade.php
+++ resources/views/day/show.blade.php
@@ -1,11 +1,19 @@
 <x-app-layout>
     <x-slot name="header" class="flex flex-row justify-between">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-bold text-xl text-gray-800 text-center dark:text-gray-200 leading-tight lg:text-2xl">
             Session du {{ $day->date->format('d/m/Y') }}
         </h2>
-        <button class="mt-2 bg-green-500 px-4 py-2 rounded-lg text-white">
-            <a href="{{ route('table.create', $day->id) }}">Ouvrir une table</a>
-        </button>
+        <div class="mt-2 flex flex-col items-center sm:flex-row sm:justify-center">
+            <x-secondary-button class="my-2 sm:my-0">
+                <img src="{{ asset('/img/game-table.png') }}" class="w-8 h-8 mr-2" alt="">
+                <a href="{{ route('table.create', $day->id) }}">Ouvrir une table</a>
+            </x-secondary-button>
+            <x-secondary-button class="ml-0 sm:ml-2">
+                <img src="{{ asset('/img/calendar.png') }}" class="w-8 h-8 mr-2" alt="">
+
+                <a href="{{ route('event.create', $day->id) }}">Créer un événement</a>
+            </x-secondary-button>
+        </div>
     </x-slot>
 
     <div class="w-full mt-2 mx-auto lg:max-w-7xl lg:px-4">
@@ -14,6 +22,15 @@
                 {{ session('error') }}
             </div>
         @endif
+
+        @if($events->count() != 0)
+            <h2 class="my-2 text-black text-xl my-2 text-center font-bold dark:text-white">Evenements</h2>
+            @foreach ($events as $event)
+                <x-event-card :event="$event"/>
+            @endforeach
+        @endif
+
+        <h2 class="text-black text-center text-xl font-bold my-2 dark:text-white">Tables disponibles</h2>
         <div class="w-full rounded-lg pt-2 mx-auto w-full sm:grid sm:grid-cols-2 sm:gap-2 lg:grid-cols-3 lg:gap-2">
             @foreach ($tables as $table)
                 <x-table-card :table="$table" />
---
+++ tests/Feature/Http/Controllers/EventControllerTest.php
@@ -0,0 +1,121 @@
+<?php
+
+use App\Models\Day;
+use App\Models\User;
+use App\Models\Event;
+
+use function Pest\Laravel\get;
+
+it('can render the event create page', function () {
+    $this->seed();
+    $this->actingAs(User::first());
+
+    $day = Day::first();
+    $response = $this->get(route('event.create', $day));
+
+    $response->assertOk();
+});
+
+it('can create a new event', function () {
+    $this->seed();
+    $this->actingAs(User::first());
+
+    $day = Day::first();
+    $response = $this->post(route('event.store', $day), [
+        'name' => 'example',
+        'description' => 'description',
+        'start_hour' => '14:00',
+    ]);
+
+    expect(['name' => 'example'])->toBeInDatabase('events');
+    expect(['description' => 'description'])->toBeInDatabase('events');
+});
+
+it('can not create an event without a name', function () {
+    $this->seed();
+    $this->actingAs(User::first());
+
+    $response = $this->post(route('event.store', Day::first()->id), [
+        'name' => '',
+        'description' => 'description',
+    ]);
+
+    expect($response)->toHaveInvalid('name');
+});
+
+it('can not create an event without a description', function () {
+    $this->seed();
+    $this->actingAs(User::first());
+
+    $response = $this->post(route('event.store', Day::first()->id), [
+        'name' => 'example',
+        'description' => '',
+    ]);
+
+    expect($response)->toHaveInvalid('description');
+});
+
+it('can not create an event without a start_hour', function () {
+    $this->seed();
+    $this->actingAs(User::first());
+
+    $response = $this->post(route('event.store', Day::first()->id), [
+        'name' => 'example',
+        'description' => 'description',
+        'start_hour' => '',
+    ]);
+
+    expect($response)->toHaveInvalid('start_hour');
+});
+
+it('can render the event edit page', function () {
+    $this->seed();
+    $this->actingAs(User::first());
+
+    $response = $this->get(route('event.edit', Event::first()));
+
+    $response->assertOk();
+});
+
+it('can update a table successfully', function () {
+    $this->seed();
+    $this->actingAs(User::first());
+
+    $response = $this->put(route('event.update', Event::first()), [
+        'name' => 'edited',
+        'description' => 'description',
+        'start_hour' => '21:00',
+    ]);
+
+    $eventUpdated = Event::first();
+
+    expect($eventUpdated->name)->toBe('edited')
+        ->and($eventUpdated->description)->toBe('description')
+        ->and($eventUpdated->start_hour)->toBe("21:00")
+        ->and($response)->toBeRedirect(route('days.show', Day::first()->id));
+
+});
+
+it('can subscribe a user to an event', function () {
+    $this->seed();
+    login();
+
+    expect(Event::first()->users->count())->toBe(1);
+
+    $this->get(route('event.subscribe', Event::first()));
+
+    expect(Event::first()->users->count())->toBe(2);
+});
+
+it('can unsubscribe a user of an event', function () {
+    $this->seed();
+    login();
+
+    $event = Event::first();
+
+    expect(Event::first()->users()->count())->toBe(1);
+
+    $this->get(route('event.unsubscribe', Event::first()));
+
+    expect(Event::first()->users()->count())->toBe(1);
+});
