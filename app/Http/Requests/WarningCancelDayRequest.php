3bb1bd99a5815d9cb3bbe558539662123952eb9d
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-06-12 09:12:03 +0200

    Rename Request validation class

--- app/Http/Requests/WarningCancelDayRequest.php
+++ app/Http/Requests/WarningCancelDayRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class WarningDayRequest extends FormRequest
+class WarningCancelDayRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
