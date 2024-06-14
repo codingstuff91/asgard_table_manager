e7118cf5aff8c66c085255343494d0a4ccb0e472
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-06-14 18:00:49 +0200

    Create Action classes

--- app/Http/Controllers/DayController.php
+++ app/Http/Controllers/DayController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Actions\Day\DisableDayAction;
 use App\Actions\Day\DeleteDayTablesAction;
+use App\Actions\Discord\CreateDayDiscordNotificationAction;
+use App\DataObjects\DiscordNotificationData;
 use App\Http\Requests\storeDayRequest;
 use App\Http\Requests\WarningCancelDayRequest;
 use App\Models\Category;
@@ -16,6 +18,12 @@ use Illuminate\Http\Request;
 
 class DayController extends Controller
 {
+    public function __construct(
+        public CreateDayDiscordNotificationAction $createDiscordNotificationAction,
+    ) {
+        //
+    }
+
     /**
      * @return View
      */
@@ -90,11 +98,16 @@ class DayController extends Controller
         return view('day.cancel', compact('day'));
     }
 
-    public function confirm_cancel(Day $day, WarningCancelDayRequest $request): RedirectResponse
-    {
+    public function confirm_cancel(
+        Day $day,
+        WarningCancelDayRequest $request,
+        CreateDayDiscordNotificationAction $createDayDiscordNotificationAction
+    ): RedirectResponse {
         app(DisableDayAction::class)->execute($day, $request->explanation);
 
         app(DeleteDayTablesAction::class)->execute($day);
+
+        ($this->createDiscordNotificationAction)($day, $request->explanation, 'cancel');
 
         return to_route('days.index');
     }
