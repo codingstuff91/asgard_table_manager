35b39a9d2436d646df1fb3a4597ba0d52fd22c43
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-08-12 21:59:50 +0200

    Add interface and abstract class

---
+++ app/Notifications/Discord/DiscordNotification.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Notifications\Discord;
+
+use App\Actions\Discord\BuildEmbedMessageStructureAction;
+use App\Actions\Discord\DefineChannelIdAction;
+use App\Actions\Discord\SendDiscordNotificationAction;
+use App\DataObjects\DiscordNotificationData;
+
+abstract class DiscordNotification implements NotificationInterface
+{
+    public function __construct(
+        public DefineChannelIdAction $defineChannelIdAction,
+        public DiscordNotificationData $discordNotificationData,
+        public SendDiscordNotificationAction $sendDiscordNotificationAction,
+        private int $channelId,
+    ) {
+    }
+
+    public function create(): void
+    {
+        $this->channelId = ($this->defineChannelIdAction)($this->discordNotificationData->day->date);
+
+        $message = $this->buildMessage($this->discordNotificationData);
+
+        $this->send($message);
+    }
+
+    abstract public function buildMessage(): array;
+
+    public function send(array $message): void
+    {
+        ($this->sendDiscordNotificationAction)($this->channelId, $message);
+    }
+}
---
+++ app/Notifications/Discord/NotificationInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Notifications\Discord;
+
+use App\DataObjects\DiscordNotificationData;
+
+interface NotificationInterface
+{
+    public function create();
+
+    public function buildMessage(): array;
+
+    public function send(array $message): void;
+
+}
