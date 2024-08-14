7162f75b186655fa95461927e17fb3b59042f96f
matthieumartin2812 <120380267+matthieumartin2812@users.noreply.github.com>
2024-08-14 22:01:12 +0200

    Fix mail config (#93)
    Co-authored-by: Matthieu MARTIN <matthieu.martinl@global-exam.com>

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'do-not-reply@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Asgard Table Manager'),
     ],
 
     /*
