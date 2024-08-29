40a710eb811a884d8dc214fc832919a395394e74
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-08-29 23:12:47 +0200

    Add createEvent method to factories file

--- tests/Factories.php
+++ tests/Factories.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Day;
+use App\Models\Event;
 
 function createDay(): Day
 {
@@ -12,4 +13,12 @@ function createPastDay(): Day
     return Day::factory()->create([
         'date' => now()->sub('day', 1),
     ]);
+}
+
+function createEvent(): Event
+{
+    return Event::factory()
+        ->create([
+            'day_id' => createDay()->id,
+        ]);
 }
