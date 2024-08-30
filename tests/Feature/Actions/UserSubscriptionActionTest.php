adec287da8aa1f2496c66864753ad5f132529530
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-08-30 15:03:44 +0200

    Apply createTable factory method

--- tests/Feature/Actions/UserSubscriptionActionTest.php
+++ tests/Feature/Actions/UserSubscriptionActionTest.php
@@ -1,22 +1,11 @@
 <?php
 
 use App\Actions\UserSubscriptionAction;
-use App\Models\Category;
-use App\Models\Day;
-use App\Models\Game;
-use App\Models\Table;
-use Illuminate\Support\Facades\Auth;
 
 test('The action class does its job correctly', function () {
     login();
 
-    $table = Table::factory()
-        ->for(Category::factory())
-        ->for(Game::factory())
-        ->for(Day::factory())
-        ->create([
-            'organizer_id' => Auth::user()->id,
-        ]);
+    $table = createTable();
 
     app(UserSubscriptionAction::class)->execute($table);
 
