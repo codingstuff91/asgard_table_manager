e9fd10468f7c15ccf94c7b758ea4fdbf3cde1ce8
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-08-30 18:26:05 +0200

    Add tests for gameController and GameRepository.php

--- app/Repositories/GameRepository.php
+++ app/Repositories/GameRepository.php
@@ -3,11 +3,22 @@
 namespace App\Repositories;
 
 use App\Models\Game;
+use Illuminate\Database\Eloquent\Collection;
 
 class GameRepository
 {
     public function findOrFail(int $id): Game
     {
         return Game::findOrFail($id);
+    }
+
+    /**
+     * @return Collection<\App\Models\Game>
+     */
+    public function findByCategory(string $categoryId): Collection
+    {
+        return Game::where('category_id', $categoryId)
+            ->orderBy('name', 'asc')
+            ->get();
     }
 }
