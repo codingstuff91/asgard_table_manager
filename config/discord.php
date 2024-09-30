48d5614dc18ce244b79551997e2a1b848e29fcf7
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-09-30 22:14:39 +0200

    Add event_channel config keys

--- config/discord.php
+++ config/discord.php
@@ -10,4 +10,6 @@ return [
 
     'bot_token' => 'Bot '.env('DISCORD_BOT_TOKEN'),
     'api_url' => 'https://discord.com/api/v10/channels/',
+    'event_channel' => 1290403039714480249,
+    'event_channel_test' => 1290404144397418579,
 ];
