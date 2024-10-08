ebd6b9b2a04ba16a5021974d5383a5ad670274db
matthieumartin2812 <120380267+matthieumartin2812@users.noreply.github.com>
2024-10-08 09:34:14 +0200

    Fix User count on admin dashboard page (#101)
    Co-authored-by: Matthieu MARTIN <matthieu.martinl@global-exam.com>

--- app/Filament/Widgets/DashboardStats.php
+++ app/Filament/Widgets/DashboardStats.php
@@ -5,6 +5,7 @@ namespace App\Filament\Widgets;
 use App\Models\Day;
 use App\Models\Game;
 use App\Models\Table;
+use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -13,7 +14,7 @@ class DashboardStats extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Utilisateurs', Table::count())
+            Stat::make('Total Utilisateurs', User::count())
                 ->icon('heroicon-o-user'),
             Stat::make('Total jeux', Game::count())
                 ->icon('heroicon-o-puzzle-piece'),
