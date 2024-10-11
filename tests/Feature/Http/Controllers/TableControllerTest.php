24c0b23edf46d14687e61c659840825878f3c914
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-10-11 16:25:12 +0200

    Create UserView test and UserControllerTest

--- tests/Feature/Http/Controllers/TableControllerTest.php
+++ tests/Feature/Http/Controllers/TableControllerTest.php
@@ -1,15 +1,12 @@
 <?php
 
-use App\Actions\UserSubscriptionAction;
 use App\Models\Category;
 use App\Models\Day;
 use App\Models\Game;
 use App\Models\Table;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Tests\RequestFactories\TableRequestFactory;
 
-use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\delete;
 use function Pest\Laravel\get;
@@ -156,119 +153,6 @@ it('Updates a table', function () {
         ->and($tableUpdated->start_hour)->toBe('15:00')
         ->and($response)->toBeRedirect(route('days.show', Day::first()->id));
 
-});
-
-it('Subscribes a user to a table', function () {
-    mockHttpClient();
-
-    $table = createTable();
-
-    get(route('table.subscribe', $table));
-
-    expect($table->users->count())->toBeOne();
-});
-
-it('Unsubscribes a user of a table', function () {
-    mockHttpClient();
-
-    $table = createTable();
-
-    get(route('table.unsubscribe', $table));
-
-    expect($table->users()->count())->toBe(0);
-});
-
-it('Can not subscribe a user already subscribed to another table with the same start hour for the same day', function () {
-    mockHttpClient();
-    $day = createDay();
-    $table = createTable(day: $day, start_hour: '21:00');
-
-    app(UserSubscriptionAction::class)->execute($table, Auth::user());
-
-    $anotherTableAtSameHour = createTable(day: $day, start_hour: '21:00');
-
-    $response = get(route('table.subscribe', $anotherTableAtSameHour));
-
-    $response->assertRedirect(route('days.show', $anotherTableAtSameHour->day));
-
-    expect($anotherTableAtSameHour->users->count())->toBe(0);
-});
-
-it('Can not subscribe a user twice or more for the same table', function () {
-    $day = createDay();
-    $table = createTable(day: $day, start_hour: '21:00');
-
-    app(UserSubscriptionAction::class)->execute($table, Auth::user());
-
-    $response = get(route('table.subscribe', $table));
-
-    expect($response)
-        ->toBeRedirect(route('days.show', $day))
-        ->toHaveSession('error');
-});
-
-test('A user can not see the edit action button for a table he didnt created', function () {
-    $day = createDay();
-
-    $anotherUser = User::factory()->create();
-
-    actingAs($anotherUser)
-        ->get(route('days.show', $day))
-        ->assertOk()
-        ->assertDontSee('img/edit.png');
-});
-
-test('An admin user can see the edit action button for a table he didnt created', function () {
-    loginAdmin();
-    $day = createDay();
-
-    createTable(day: $day);
-
-    get(route('days.show', $day))
-        ->assertOk()
-        ->assertSee('img/edit.png');
-});
-
-test('A user can not render the edit page for a table he didnt create', function () {
-    $table = createTable();
-
-    $anotherUser = User::factory()->create();
-
-    actingAs($anotherUser)
-        ->get(route('table.edit', $table))
-        ->assertForbidden();
-});
-
-test('An admin user can render the edit page for a table he didnt create', function () {
-    loginAdmin();
-
-    $table = createTable();
-
-    get(route('table.edit', $table))->assertOk();
-});
-
-test('A user can not see the delete action button for a table he didnt created', function () {
-    $day = createDay();
-
-    $anotherUser = User::factory()->create();
-
-    createTable(day: $day);
-
-    actingAs($anotherUser)
-        ->get(route('days.show', $day))
-        ->assertOk()
-        ->assertDontSee('img/delete.png');
-});
-
-test('An admin user can see the delete action button for a table he didnt created', function () {
-    loginAdmin();
-    $day = createDay();
-
-    createTable(day: $day);
-
-    get(route('days.show', $day))
-        ->assertOk()
-        ->assertSee('img/delete.png');
 });
 
 test('Deletes a table', function () {
---
+++ tests/Feature/Http/Controllers/UserControllerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Actions\UserSubscriptionAction;
+use Illuminate\Support\Facades\Auth;
+
+use function Pest\Laravel\get;
+
+beforeEach(function () {
+    login();
+});
+
+it('Subscribes a user to a table', function () {
+    mockHttpClient();
+
+    $table = createTable();
+
+    get(route('table.subscribe', $table));
+
+    expect($table->users->count())->toBeOne();
+});
+
+it('Unsubscribes a user of a table', function () {
+    mockHttpClient();
+
+    $table = createTable();
+
+    get(route('table.unsubscribe', $table));
+
+    expect($table->users()->count())->toBe(0);
+});
+
+it('Can not subscribe a user already subscribed to another table with the same start hour for the same day',
+    function () {
+        mockHttpClient();
+        $day = createDay();
+        $table = createTable(day: $day, start_hour: '21:00');
+
+        app(UserSubscriptionAction::class)->execute($table, Auth::user());
+
+        $anotherTableAtSameHour = createTable(day: $day, start_hour: '21:00');
+
+        $response = get(route('table.subscribe', $anotherTableAtSameHour));
+
+        $response->assertRedirect(route('days.show', $anotherTableAtSameHour->day));
+
+        expect($anotherTableAtSameHour->users->count())->toBe(0);
+    });
+
+it('Can not subscribe a user twice or more for the same table', function () {
+    $day = createDay();
+    $table = createTable(day: $day, start_hour: '21:00');
+
+    app(UserSubscriptionAction::class)->execute($table, Auth::user());
+
+    $response = get(route('table.subscribe', $table));
+
+    expect($response)
+        ->toBeRedirect(route('days.show', $day))
+        ->toHaveSession('error');
+});
