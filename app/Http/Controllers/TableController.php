3767c42cf1600c9a7cb5ef54f022f98636de27fa
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2024-11-07 08:58:19 +0100

    Edit code style

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -131,8 +131,12 @@ class TableController extends Controller
 
             $discordNotificationData = $this->discordNotificationData::make($table->game, $table, $table->day);
 
-            $discordNotification = ($this->notificationFactory)(entity: 'table', type: 'cancel-table',
-                discordNotificationData: $discordNotificationData);
+            $discordNotification = ($this->notificationFactory)(
+                entity: 'table',
+                type: 'cancel-table',
+                discordNotificationData: $discordNotificationData
+            );
+
             $discordNotification->handle();
 
         } catch (Exception $e) {
