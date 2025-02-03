d8cfe3d657731be82ac6c1f88a58bd2f7b91fa3e
matthieumartin2812 <120380267+matthieumartin2812@users.noreply.github.com>
2025-02-03 13:44:14 +0100

    Changed authentication process (#118)
    * Add new Model and pivot table creation migration
    
    * Add new relation
    
    * Fix migration
    
    * Add Factory and Seeder for Association model
    
    * Change page title layout
    
    * add associations route into guest middleware
    
    * add feature test for null association url parameter
    
    * Add association select logic
    
    * Fix test
    
    * Change controller and route
    
    * Edit factory and seeder
    
    * Create new view and association choosing card component
    
    * Associate user to an association
    
    * Add test
    
    * Add test
    
    * Add regularization command
    
    * Fix PHPStan errors
    
    * Create BaseStorage and AssociationStorage
    
    * Store user association into session after user login
    
    * Fix PHPstan error
    
    ---------
    
    Co-authored-by: Matthieu MARTIN <matthieu.martinl@global-exam.com>

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
+use App\Storages\AssociationStorage;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -25,7 +26,11 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        $user = $request->authenticate();
+
+        $association = $user->associations->first();
+
+        AssociationStorage::put($association);
 
         $request->session()->regenerate();
 
