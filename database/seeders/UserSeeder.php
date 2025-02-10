f6ba95824ad0d6e73771af3a425c8e81f1cf42ca
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-02-10 14:25:54 +0100

    Update UserSeeder to associate to an Association

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Association;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +15,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create([
-            'name' => 'mattou',
-            'email' => 'mattou2812@gmail.com',
-            'admin' => true,
-        ]);
+        $association = Association::first() ?? Association::factory()->create();
+
+        User::factory()
+            ->has(Association::factory())
+            ->create([
+                'name' => 'mattou',
+                'email' => 'mattou2812@gmail.com',
+                'admin' => true,
+            ]);
     }
 }
