86da6a326b72c23d2fbd36ee86ed90b9d1df6001
Matthieu MARTIN <matthieu.martinl@global-exam.com>
2025-02-10 14:38:37 +0100

    Update stats to include current association

--- tests/Feature/Http/Controllers/DashboardControllerTest.php
+++ tests/Feature/Http/Controllers/DashboardControllerTest.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Models\Association;
+use App\Storages\AssociationStorage;
+
 use function Pest\Laravel\get;
 
 beforeEach(function () {
     login();
+
+    $association = Association::factory()->create();
+    AssociationStorage::put($association);
 });
 
 test('It renders the index page', function () {
