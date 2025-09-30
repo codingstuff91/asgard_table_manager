4081a3c16bf1adb3586be29605ee3454f7379196
Matthieu Martin <matthieu.martin@global-exam.com>
2025-09-30 13:10:40 +0200

    Delete gate for edit an event

--- resources/views/components/event-card-header.blade.php
+++ resources/views/components/event-card-header.blade.php
@@ -4,30 +4,28 @@
             <h2 class="text-lg font-bold">{{ $event->start_hour }}</h2>
         </div>
         <div class="px-4 flex justify-end items-center">
-            @can('edit_event')
-                <button class="mr-2 h-6 w-6">
-                    <a
-                        href="{{ route('event.edit', $event->id) }}"
-                        onclick="return confirm('Etes-vous certain de vouloir éditer cet évènement ?')"
-                    >
-                        <img src="{{ asset('img/edit.png')}}" class="h-6 w-6">
-                    </a>
-                </button>
-            @endcan
+            <button class="mr-2 h-6 w-6">
+                <a
+                    href="{{ route('event.edit', $event->id) }}"
+                    onclick="return confirm('Etes-vous certain de vouloir éditer cet évènement ?')"
+                >
+                    <img src="{{ asset('img/edit.png')}}" class="h-6 w-6">
+                </a>
+            </button>
 
-                @can('delete_event')
-                    <form action="{{ route('event.destroy', $event->id) }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button
-                            type="submit"
-                            class="ml-2 h-6 w-6"
-                            onclick="return confirm('Etes-vous certain de vouloir annuler cet évènement ?')"
-                        >
-                            <img src="{{ asset('img/delete.png')}}">
-                        </button>
-                    </form>
-                @endcan
+            @can('delete_event')
+                <form action="{{ route('event.destroy', $event->id) }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button
+                        type="submit"
+                        class="ml-2 h-6 w-6"
+                        onclick="return confirm('Etes-vous certain de vouloir annuler cet évènement ?')"
+                    >
+                        <img src="{{ asset('img/delete.png')}}">
+                    </button>
+                </form>
+            @endcan
         </div>
     </div>
 
