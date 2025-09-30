cc558ed4158514fabfdd967f1d0def32e4a36083
matthieumartin2812 <120380267+matthieumartin2812@users.noreply.github.com>
2025-09-30 13:27:57 +0200

    Updated reset password page (#128)
    * Update PR template
    
    * Update reset password page
    
    ---------
    
    Co-authored-by: Matthieu MARTIN <matthieu.martinl@global-exam.com>

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -14,25 +14,31 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Mot de passe')"/>
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirmer le mot de passe')"/>
+            <x-input-label
+                for="password_confirmation"
+                :value="__('Confirmer le mot de passe')"
+            />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
+            <x-text-input
+                id="password_confirmation"
+                class="block mt-1 w-full"
+                type="password"
+                name="password_confirmation" required
+            />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Reinitialiser le mot de passe') }}
+                {{ __('Confirmer') }}
             </x-primary-button>
         </div>
     </form>
